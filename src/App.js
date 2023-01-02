2f0bc95cbb9166455d623255bd636b44c17c854b
Farouk adedamola <faroukadedamola1@gmail.com>
2023-01-02 15:08:25 +0100

    commit font files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function App() {
-  return <div className="App">GPT -3</div>;
+  return (
+    <div style={{ fontWeight: "800", fontFamily: "manrope" }}>
+      GPT -3
+      <p>
+        Already screened the selar builder and design implementation is
+        limited.. design will be perfect but not something out of the blue sir
+        unlike working with a dedicated fully functional builder.
+      </p>
+    </div>
+  );
 }
 
 export default App;
