528a44036fdc023d60c75cb9c8996aa18b6c2908
Farouk adedamola <faroukadedamola1@gmail.com>
2023-01-05 00:50:24 +0100

    commit what is Gpt

--- src/Components/WhatisGPT/Gpt.js
+++ src/Components/WhatisGPT/Gpt.js
@@ -1,40 +1,51 @@
 import React, { Fragment } from "react";
 import classes from "./Gpt.module.css";
 import { main, child } from "./Gptdata";
+import { Link } from "react-router-dom";
 
 const Gpt = () => {
   return (
     <Fragment>
-      <section className={classes.gpt__container}>
-        <div className={classes.gpt__main}>
-          {main.map((content) => {
-            const { id, title, text } = content;
-            return (
-              <div key={id} className={classes.gpt__main__container}>
-                <div className={classes.gpt__overline}></div>
-                <h1>{title}</h1>
-                <p>{text}</p>
-              </div>
-            );
-          })}
-        </div>
-        <div className={classes.gpt__explore}>
-          <h1>The possibilities are beyond your imagination</h1>
-          <p>Explore The Library</p>
-        </div>
-        <div className={classes.gpt__child}>
-          {child.map((content) => {
-            const { id, title, text } = content;
-            return (
-              <div key={id}>
-                <div className={classes.gpt__overline}></div>
-                <h1>{title}</h1>
-                <p>{text}</p>
-              </div>
-            );
-          })}
-        </div>
-      </section>
+      <article className={classes.gpt}>
+        <section className={classes.gpt__container}>
+          <div className={classes.gpt__main}>
+            {main.map((content) => {
+              const { id, title, text } = content;
+              return (
+                <div key={id} className={classes.gpt__main__container}>
+                  <div className={classes.gpt__main__container__title}>
+                    <div className={classes.gpt__overline}></div>
+                    <h1>{title}</h1>
+                  </div>
+                  <p>{text}</p>
+                </div>
+              );
+            })}
+          </div>
+          <div className={classes.gpt__explore}>
+            <h1>The possibilities are beyond your imagination</h1>
+            <p>
+              <Link to="home" className={classes.gpt__explore__link}>
+                Explore The Library
+              </Link>
+            </p>
+          </div>
+          <div className={classes.gpt__child}>
+            {child.map((content) => {
+              const { id, title, text } = content;
+              return (
+                <div key={id} className={classes.gpt__child__container}>
+                  <div className={classes.gpt__child__container__title}>
+                    <div className={classes.gpt__overline}></div>
+                    <h1>{title}</h1>
+                  </div>
+                  <p>{text}</p>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      </article>
     </Fragment>
   );
 };
