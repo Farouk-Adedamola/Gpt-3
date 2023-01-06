30c2bfa38e12a82bfb2bb206468b57f9b13d4d8d
Farouk adedamola <faroukadedamola1@gmail.com>
2023-01-06 10:54:28 +0100

    commit styles for myapps

--- src/Components/MyApps/MyApps.js
+++ src/Components/MyApps/MyApps.js
@@ -1,17 +1,19 @@
 import React, { Fragment } from "react";
 import classes from "./MyApps.module.css";
-import vr from "../../Assets/Images/VirtualRel.svg";
+import vr from "../../Assets/Images/Vr.png";
 import { Link } from "react-router-dom";
 
 const MyApps = () => {
   return (
     <Fragment>
-      <section>
-        <div>
+      <section className={classes.myapps__section}>
+        <div className={classes.section__one}>
           <img src={vr} alt="virtual reality" />
         </div>
-        <div>
-          <Link to="/">Request Early Access to Get Started</Link>
+        <div className={classes.section__two}>
+          <Link to="/" className={classes.change}>
+            Request Early Access to Get Started
+          </Link>
           <h1>The possibilities are beyond your imagination</h1>
           <p>
             Yet bed any for travelling assistance indulgence unpleasing. Not
